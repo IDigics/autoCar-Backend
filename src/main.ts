60eb7600919aefde387bdf3d428596596194510d
Wissem Hajbi <wissemhajbi2002@gmail.com>
2025-07-27 22:40:37 +0200

    Hard code the frontend origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     app.enableCors({
-    origin: process.env.ORIGIN, // * to be changed to and ip adress or domain name
+    origin: "http://localhost:3001", // * to be changed to and ip adress or domain name
     methods: 'GET,POST,PUT,PATCH,DELETE',
     credentials: true,
   })
