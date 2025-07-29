d1d14da84ca7c27a48c7f4bda98a83d37d8e00ba
KhaledCherif <cherifkhaled.tn@gmail.com>
2025-07-29 09:46:28 +0100

    images now are served

---
+++ src/car-image/car-image.controller.ts
@@ -0,0 +1,29 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Res,
+  NotFoundException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Response } from 'express';
+import { join } from 'path';
+import { createReadStream, existsSync } from 'fs';
+
+@Controller('image')
+export class CarImageController {
+  constructor(private configService: ConfigService) {}
+
+  @Get(':filename')
+  async serveImage(@Param('filename') filename: string, @Res() res: Response) {
+    const uploadFolder = this.configService.get<string>('UPLOAD_FOLDER') || 'uploads/car-images';
+    const filePath = join(__dirname, '..', '..', uploadFolder, filename);
+
+    if (!existsSync(filePath)) {
+      throw new NotFoundException('Image not found');
+    }
+
+    const stream = createReadStream(filePath);
+    stream.pipe(res);
+  }
+}
