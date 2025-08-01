9c929070f533c5a631abb578aa5192d6356b904c
slim-chahed <137616260+slim-chahed@users.noreply.github.com>
2025-08-01 13:38:40 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -12,11 +12,13 @@ async function bootstrap() {
     credentials: true,
   });
 
-  const uploadFolder = process.env.UPLOAD_FOLDER || '/app/uploads';
-  app.useStaticAssets(join(__dirname, '..', uploadFolder), {
-    prefix: '/image/',
-  });
-
+  //const uploadFolder = process.env.UPLOAD_FOLDER || '/app/uploads';
+  //app.useStaticAssets(join(__dirname, '..', uploadFolder), {
+   // prefix: '/image/',
+ // });
+app.use('/image', express.static('/app/uploads', {
+  redirect: false  // Prevent automatic redirects
+}));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
